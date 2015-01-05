ba8f8e57c1084426274b0a8327c406d9430262f7
Dan Carter <dcarter@adharmonics.com>
2015-01-04 23:51:11 -0500

    Add name query param

--- hello_world.go
+++ hello_world.go
@@ -8,7 +8,11 @@ import (
 type HomePageHandler struct{}
 
 func (h *HomePageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintln(w, "Hello World!")
+  name := r.URL.Query().Get("name")
+  if name == "" {
+    name = "World"
+  }
+  fmt.Fprintf(w, "Hello %s!", name)
 }
 
 func main() {
