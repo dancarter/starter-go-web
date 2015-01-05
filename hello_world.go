1fde892c3711dfdd30051712c74973f9cf21dc97
Dan Carter <dcarter@adharmonics.com>
2015-01-04 23:46:21 -0500

    Add HomePageHandler and remove /bar

--- hello_world.go
+++ hello_world.go
@@ -5,16 +5,14 @@ import (
   "net/http"
 )
 
+type HomePageHandler struct{}
+
+func (h *HomePageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintln(w, "Hello World!")
+}
+
 func main() {
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Hello World!")
-  })
-
-  barHandler := func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Hello Bar!")
-  }
-
-  http.HandleFunc("/bar", barHandler)
+  http.Handle("/", &HomePageHandler{})
 
   http.ListenAndServe(":3000", nil)
 }
