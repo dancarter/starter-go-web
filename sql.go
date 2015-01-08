06c41527b4255fcada714a3780073220383e1feb
Dan Carter <dcarter@adharmonics.com>
2015-01-07 22:48:51 -0500

    Add sqlx package

--- sql.go
+++ sql.go
@@ -4,37 +4,48 @@ import (
   // _ imports this package anonymously so we don't get an error
   // from not explicitly using it
   _ "github.com/lib/pq"
-  "database/sql"
+  "github.com/jmoiron/sqlx"
   "log"
   "time"
 )
 
 type Todo struct {
-  id          int
-  subject     string
-  description string
-  completed   bool
-  created_at  time.Time
-  updated_at  time.Time
+  Id          int
+  Subject     string
+  Description string
+  Completed   bool
+  CreatedAt   time.Time `db:"created_at"`
+  UpdatedAt   time.Time `db:"updated_at"`
 }
 
 func main() {
-  db, err := sql.Open("postgres", "postgres://postgres:postgres@localhost/godos_development?sslmode=disable")
+  db, err := sqlx.Open("postgres", "postgres://postgres:postgres@localhost/godos_development?sslmode=disable")
   if err != nil {
     log.Fatal(err)
   }
 
-  rows, err := db.Query("select * from todos")
-  for rows.Next() {
-    todo := Todo{}
-    if err := rows.Scan(&todo.id, &todo.subject, &todo.description, &todo.completed, &todo.created_at, &todo.updated_at); err != nil {
-      log.Fatal(err)
-    }
-    log.Printf("Id is %s", todo.id)
-    log.Printf("Subject is %s", todo.subject)
+  // Start db transaction
+  tx := db.MustBegin()
+  now := time.Now()
+  t := Todo{
+    Subject:      "Mow Lawn",
+    Description:  "Yuck!",
+    CreatedAt:    now,
+    UpdatedAt:    now,
+  }
+  tx.Exec("insert into todos (subject, description, created_at, updated_at) values $1, $2, $3, $4", t.Subject, t.Description, t.CreatedAt, t.UpdatedAt)
+  // This is an invalid record that will rollback the transaction
+  tx.Exec("insert into todos (subject, description, created_at, updated_at) values $1, $2, $3, $4", nil, t.Description, t.CreatedAt, t.UpdatedAt)
+  // End db transaction
+  tx.Commit()
+
+  todos := []Todo{}
+  err = db.Select(&todos, "select * from todos")
+  if err != nil {
+    log.Fatal(err)
   }
 
-  if err := rows.Err(); err != nil {
-    log.Fatal(err)
+  for _, todo := range todos {
+    log.Printf("Subject is %s", todo.Subject)
   }
 }
